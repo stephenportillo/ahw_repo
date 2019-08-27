729702661db359a128ec54c46442b1bdb3000f10
Stephen Portillo <sportill@uw.edu>
2019-08-27 04:12:37 -0700

    Added init file and setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ahw_repo',
+      version='0.0.1',
+      description='convenience functions for PSF photometry',
+      author='Stephen Portillo',
+      author_email='sportill@uw.edu',
+      packages=['ahw_repo'],
+     ) #complete
